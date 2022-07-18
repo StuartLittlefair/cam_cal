31b4db81700a87ca11972e3c90eec3fc98ffb092
Alex-J-Brown <ajbrown2@sheffield.ac.uk>
2022-07-18 09:09:34 +0100

    Shorter header comments to fix Issue #3

--- cam_cal/fits.py
+++ cam_cal/fits.py
@@ -28,37 +28,37 @@ class FITS:
         header = fits.Header()
         for key, value in header_dict.items():
             header.set(key, value)
-        header.comments['TARGET'] = "Name of target."
-        header.comments['RUN'] = "Imaging run of target."
-        header.comments['FILTERS'] = "Filters installed."
-        header.comments['NIGHT'] = "Date (UTC) at start of night."
-        header.comments['INSTR'] = "Instrument."
+        header.comments['TARGET'] = "Name of target"
+        header.comments['RUN'] = "Imaging run of target"
+        header.comments['FILTERS'] = "Filters installed"
+        header.comments['NIGHT'] = "Date (UTC) at start of night"
+        header.comments['INSTR'] = "Instrument"
         header.comments['RA'] = "Right ascension (deg)"
         header.comments['DEC'] = "Declination (deg)"
-        header.comments['TAR_AIRM'] = "Mean airmass of target during run."
-        header.comments['STD_STAR'] = "Standard star used for calibration."
-        header.comments['STD_RUN'] = "Imaging run of standard star."
-        header.comments['STD_AIRM'] = "Mean airmass of standard during run."
-        header.comments['ZP'] = "Calculated zeropoints."
-        header.comments['ZP_E'] = "Uncertainty of calculated zeropoints."
-        header.comments['ATM_EX'] = "Atmospheric extinctions."
-        header.comments['ATM_EX_E'] = "Uncertainty of atmospheric extinctions."
-        header.comments['TIME_CAL'] = "Time data was calibrated (UTC)."
-        header.comments['TIME_ID'] = "Unique ID of calibration."
+        header.comments['TAR_AIRM'] = "Mean airmass of target during run"
+        header.comments['STD_STAR'] = "Standard star used for calibration"
+        header.comments['STD_RUN'] = "Imaging run of standard star"
+        header.comments['STD_AIRM'] = "Mean airmass of standard during run"
+        header.comments['ZP'] = "Calculated zeropoints"
+        header.comments['ZP_E'] = "Zeropoint uncertainties"
+        header.comments['ATM_EX'] = "Atmospheric extinctions"
+        header.comments['ATM_EX_E'] = "Extinction Uncertainties"
+        header.comments['TIME_CAL'] = "Time data was calibrated (UTC)"
+        header.comments['TIME_ID'] = "Unique ID of calibration"
         primary_hdu = fits.PrimaryHDU(header=header)
         hdul = fits.HDUList([primary_hdu])
         for filt in data_dict['header'].keys():
             hdr = fits.Header()
             for key, value in data_dict['header'][filt].items():
                 hdr.set(key, value)
-            hdr.comments['FILTER'] = "Filter used."
-            hdr.comments['COMP_AP'] = "Aperture used for comparison star."
-            hdr.comments['FC_E'] = "Flux calibration fractional uncertainty."
-            hdr.comments['FC_MAG_E'] = "Flux calibration uncertainty (magnitudes)."
-            hdr.comments['ATM_EX'] = "Atmospheric extinction."
-            hdr.comments['ATM_EX_E'] = "Uncertainty in atmospheric extinction."
-            hdr.comments['ZP'] = "Zeropoint."
-            hdr.comments['ZP_E'] = "Uncertainty in zeropoint."
+            hdr.comments['FILTER'] = "Filter used"
+            hdr.comments['COMP_AP'] = "Aperture used for comparison star"
+            hdr.comments['FC_E'] = "Flux calibration fractional uncertainty"
+            hdr.comments['FC_MAG_E'] = "Flux calibration uncertainty (magnitudes)"
+            hdr.comments['ATM_EX'] = "Atmospheric extinction"
+            hdr.comments['ATM_EX_E'] = "Uncertainty in atmospheric extinction"
+            hdr.comments['ZP'] = "Zeropoint"
+            hdr.comments['ZP_E'] = "Zeropoint uncertainty"
             BMJD = fits.Column(name='BMJD(TDB)', format='D', array=data_dict['data'][filt]['BMJD(TDB)'], unit='d')
             Exp_time = fits.Column(name='Exp_time', format='D', array=data_dict['data'][filt]['exp_time'], unit='d')
             Flux = fits.Column(name='Flux', format='D', array=data_dict['data'][filt]['flux'], unit='Jansky')
@@ -74,7 +74,7 @@ class FITS:
         self.hdul.writeto(fname, output_verify='warn', overwrite=True)
 
     
-    def to_lcurve(self, filename=None, filepath=None):
+    def to_lcurve(self, filename=None, filepath=None, tname=None):
         header = self.hdul[0].header
         for i, hdu in enumerate(self.hdul[1:]):
             hdr = hdu.header
@@ -90,7 +90,9 @@ class FITS:
             fname = filename
             fpath = filepath
             if not fname:
-                fname = f"{header['TARGET']}_{header['RUN']}_{header['TIME_ID']}_{hdr['FILTER']}_fc.dat"
+                if not tname:
+                    tname = header['TARGET']
+                fname = f"{tname}_{header['RUN']}_{header['TIME_ID']}_{hdr['FILTER']}_fc.dat"
                 if not fpath:
                     try:
                         fpath = os.path.dirname(self.fname)
